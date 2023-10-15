d33c75fa4ae35cdb58d5a2470c1d1c53d0292f1f
EnzoCortez <enzo.cortez@udla.edu.ec>
2023-10-15 18:00:21 -0500

    Solicitud de datos por teclado

---
+++ Divisores.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/*Leer un número entero 
+y mostrar todos los divisores exactos del número comprendidos entre 1 y el número leído.*/
+
+int num;
+
+int main(){
+
+    printf("-----------Menu------------- \n" );
+    printf("Ingrese un valor: ");
+    scanf("%d",&num);
+
+}
