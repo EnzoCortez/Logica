4749e4888cac151c86f6a80e2b841d34d04c5611
EnzoCortez <enzo.cortez@udla.edu.ec>
2023-10-15 18:58:43 -0500

    Configuracion inicial

---
+++ Identificacion.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/*Leer un número entero de 3 dígitos y determinar si tiene el dígito 1.*/
+
+
+
+int main() {
+    int numero;
+
+    printf("Ingrese un número entero de 3 dígitos: ");
+    scanf("%d", &numero);
+    
+    return 0;
+}
