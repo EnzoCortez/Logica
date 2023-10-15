503ed91c13889159c775bd86ac5d6de96d25c6a4
EnzoCortez <enzo.cortez@udla.edu.ec>
2023-10-15 18:26:51 -0500

    Calculo de los divisores

--- Divisores.c
+++ Divisores.c
@@ -3,6 +3,8 @@
 y mostrar todos los divisores exactos del número comprendidos entre 1 y el número leído.*/
 
 int num;
+int i;
+int div;
 
 int main(){
 
@@ -10,4 +12,11 @@ int main(){
     printf("Ingrese un valor: ");
     scanf("%d",&num);
 
+    for (i=1;i<=num;i++){    
+    div=num/i;
+        if (div !=0)
+        {
+            printf("El número %d no es divisor exacto de %d\n", i, num);
+        }
+    }
 }
