4cf54efde6d7be428c7d8ffb80e6cf3ae426d577
EnzoCortez <enzo.cortez@udla.edu.ec>
2023-10-15 18:43:52 -0500

    Validacion de datosó

--- Divisores.c
+++ Divisores.c
@@ -9,17 +9,22 @@ int div;
 int main(){
 
     printf("-----------Menu------------- \n" );
-    printf("Ingrese un valor: ");
+    printf("Ingrese un valor entero: ");
     scanf("%d",&num);
 
-    for (i=1;i<=num;i++){    
-    div=num%i;
-        if (div !=0)
-        {
-            
-        }
-        else{
-            printf("El número %d es divisor exacto de %d\n", i, num);
-        }
+    if (num<1){
+        printf("Valor no valido intente de nuevo");
+    }
+    else{
+        for (i=1;i<=num;i++){    
+            div=num%i;
+                if (div !=0)
+                {
+                    
+                }
+                else{
+                    printf("El número %d es divisor exacto de %d\n", i, num);
+                }
+            }
     }
 }
