55fe268fac3b71f868b1a7cfc9e6507d200ca4d2
EnzoCortez <enzo.cortez@udla.edu.ec>
2023-10-15 19:18:03 -0500

    Configuracion inicial

---
+++ Multiplos.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*Leer un número entero y mostrar todos sus componentes numéricos, o sea, aquellos para quienes él sea un múltiplo.*/
+
+
+int main() {
+    int numero;
+
+    printf("Ingrese un número entero: ");
+    scanf("%d", &numero);
+
+    return 0;
+}
