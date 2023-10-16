642235134ac9b7707f22a6c0a00ac98550092bea
EnzoCortez <enzo.cortez@udla.edu.ec>
2023-10-15 19:09:10 -0500

    Configuracion de busqueda inicial

---
+++ Suma.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+/*Leer un número entero y determinar a cuánto es igual la suma de sus dígitos.*/
+
+int main(){
+    int numero, digito, suma = 0;
+    printf("Ingrese un número entero: ");
+    scanf("%d", &numero);
+}
