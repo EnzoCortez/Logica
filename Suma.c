3171508648be548f3c9b0cf55af5d4bec2578e3d
EnzoCortez <enzo.cortez@udla.edu.ec>
2023-10-15 19:11:00 -0500

    Calculo de los elementos

--- Suma.c
+++ Suma.c
@@ -6,4 +6,10 @@ int main(){
     int numero, digito, suma = 0;
     printf("Ingrese un número entero: ");
     scanf("%d", &numero);
+
+    while (numero != 0) {
+        digito = numero % 10; // Obtiene el último dígito
+        suma += digito; // Agrega el dígito a la suma total
+        }
+
 }
